63f5eef857ea54ac9315c1bb57f30dd7099e7646
LuxerIThink <eliteluxer@gmail.com>
2023-07-01 04:47:52 +0200

    Add email min length

--- api/models/user.py
+++ api/models/user.py
@@ -4,6 +4,6 @@ from .abstract import AbstractBaseModel
 
 class User(AbstractBaseModel):
     username = fields.CharField(min_length=3, max_length=32, unique=True)
-    password = fields.CharField(min_length=8, max_length=128)
-    email = fields.CharField(max_length=128, unique=True)
+    password = fields.CharField(min_length=8, max_length=64)
+    email = fields.CharField(min_length=3, max_length=64, unique=True)
     devices = fields.ReverseRelation["Device"]
