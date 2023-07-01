a65bd986910f11686e5d06c9a0b7706569bfe553
LuxerIThink <eliteluxer@gmail.com>
2023-07-01 04:50:03 +0200

    Fix foreign key

--- api/models/device.py
+++ api/models/device.py
@@ -1,13 +1,14 @@
 from tortoise import fields
 from .abstract import AbstractBaseModel
+from .user import User
 
 
 class Device(AbstractBaseModel):
-    name = fields.CharField(max_length=32, unique=True)
-    user = fields.ForeignKeyField(
-        "models.User",
-        related_name="devices",
-        on_delete=fields.CASCADE
+    name = fields.CharField(min_length=2, max_length=32)
+    user: fields.ForeignKeyRelation[User] = fields.ForeignKeyField(
+        "api.User",
+        "devices",
+        fields.CASCADE
     )
     is_read_public = fields.BooleanField(default=False)
     sensors = fields.ReverseRelation["Sensor"]
--- api/models/sensor.py
+++ api/models/sensor.py
@@ -1,13 +1,13 @@
 from tortoise import fields
 from .abstract import AbstractBaseModel
+from .device import Device
 
 
 class Sensor(AbstractBaseModel):
-    name = fields.CharField(max_length=32)
-    device = fields.ForeignKeyField(
-        "models.Device",
-        related_name="sensors",
-        on_delete=fields.CASCADE
-    )
-    unit = fields.CharField(max_length=32)
+    name = fields.CharField(min_length=2, max_length=32)
+    device: fields.ForeignKeyRelation[Device] = fields.ForeignKeyField(
+        "api.Device",
+        "sensors",
+        fields.CASCADE)
+    unit = fields.CharField(max_length=32, null=True)
     measurements = fields.ReverseRelation["Measurement"]
