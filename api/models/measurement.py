bff296ee4368e3f9523cdf9325d0aec36e2191dc
LuxerIThink <eliteluxer@gmail.com>
2023-07-01 04:49:34 +0200

    Add measurement model

---
+++ api/models/measurement.py
@@ -0,0 +1,13 @@
+from tortoise import fields
+from .abstract import AbstractBaseModel
+from .sensor import Sensor
+
+
+class Measurement(AbstractBaseModel):
+    sensor: fields.ForeignKeyRelation[Sensor] = fields.ForeignKeyField(
+        "api.Sensor",
+        "measurements",
+        fields.CASCADE
+    )
+    time = fields.DatetimeField()
+    value = fields.FloatField()
