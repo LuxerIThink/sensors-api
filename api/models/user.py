e2f336212cca8438e1c8613fabf9802c67e9ad92
LuxerIThink <eliteluxer@gmail.com>
2023-07-11 01:11:30 +0200

    :sparkles: Add pydantic models

--- api/models/user.py
+++ api/models/user.py
@@ -1,9 +1,22 @@
 from tortoise import fields
 from .abstract import AbstractBaseModel
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 
 class User(AbstractBaseModel):
-    username = fields.CharField(min_length=3, max_length=32, unique=True)
-    password = fields.CharField(min_length=8, max_length=64)
-    email = fields.CharField(min_length=3, max_length=64, unique=True)
+    username = fields.CharField(max_length=32, unique=True)
+    password = fields.TextField(max_length=128)
+    email = fields.CharField(max_length=32, unique=True)
     devices = fields.ReverseRelation["Device"]
+
+
+UserInPydantic = pydantic_model_creator(
+    User,
+    name="UserIn",
+    exclude_readonly=True,
+)
+UserOutPydantic = pydantic_model_creator(
+    User,
+    name="UserOut",
+    exclude=("password",)
+)
