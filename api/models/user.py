f9918b225488b690db26c9cff88fc4568cff1393
LuxerIThink <eliteluxer@gmail.com>
2023-07-11 02:07:49 +0200

    :sparkles: Add password hashing

--- api/models/user.py
+++ api/models/user.py
@@ -1,6 +1,7 @@
 from tortoise import fields
 from .abstract import AbstractBaseModel
 from tortoise.contrib.pydantic import pydantic_model_creator
+from ..startup.security import password_hasher
 
 
 class User(AbstractBaseModel):
@@ -8,6 +9,10 @@ class User(AbstractBaseModel):
     password = fields.TextField(max_length=128)
     email = fields.CharField(max_length=32, unique=True)
     devices = fields.ReverseRelation["Device"]
+
+    async def save(self, *args, **kwargs):
+        self.password = password_hasher.hash(self.password)
+        await super().save(*args, **kwargs)
 
 
 UserInPydantic = pydantic_model_creator(
