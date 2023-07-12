844d2432e679dee91330803d84bb73a09c3d6dac
LuxerIThink <eliteluxer@gmail.com>
2023-07-12 02:36:04 +0200

    :wrench: Add validator exceptions handler

---
+++ api/handlers/exceptions_handlers.py
@@ -0,0 +1,13 @@
+from starlette.responses import JSONResponse
+
+
+async def validation_exception_handler(_, exc):
+    error = {
+        "loc": [str(exc).split(':')[0], 1],
+        "msg": str(exc),
+        "type": "validation"
+    }
+    return JSONResponse(
+        status_code=422,
+        content={"detail": [error]}
+    )
--- api/main.py
+++ api/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+
+from .handlers.exceptions_handlers import validation_exception_handler
 from .routers import actions, users
 from .startup.orm import connect_to_db
+from tortoise.exceptions import ValidationError
+
 
 app = FastAPI()
 
@@ -8,3 +12,5 @@ connect_to_db(app)
 
 app.include_router(actions.router)
 app.include_router(users.router)
+
+app.add_exception_handler(ValidationError, validation_exception_handler)
