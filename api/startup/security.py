c11907a4e8a320147539356fe8771061c41750f0
LuxerIThink <eliteluxer@gmail.com>
2023-07-20 23:05:30 +0200

    :closed_lock_with_key: Add JWT salt

--- api/startup/security.py
+++ api/startup/security.py
@@ -1,3 +1,4 @@
+from os import getenv
 from argon2 import PasswordHasher
 from fastapi.security import OAuth2PasswordBearer
 from password_strength import PasswordPolicy
@@ -13,3 +14,5 @@ password_checker = PasswordPolicy.from_names(
 )
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="actions/token")
+
+salt = str(getenv("JWT_SECRET"))
