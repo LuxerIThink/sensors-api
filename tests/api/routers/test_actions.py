3cd536bf2a5f907168878d8bc58153a6e2e0da6f
LuxerIThink <eliteluxer@gmail.com>
2023-07-28 01:59:18 +0200

    :white_check_mark: Add more actions tests

--- tests/api/routers/test_actions.py
+++ tests/api/routers/test_actions.py
@@ -28,3 +28,9 @@ class TestActions:
         response = client.post("/actions/token/", data=login_data, headers=header)
 
         assert response.status_code == 200
+
+    async def test_not_existing_login_in(self, client, correct_user_data):
+        login_data = {key: value for key, value in correct_user_data.items() if key != "email"}
+        header = {"Content-Type": "application/x-www-form-urlencoded"}
+        response = client.post("/actions/token/", data=login_data, headers=header)
+        assert response.status_code == 404
