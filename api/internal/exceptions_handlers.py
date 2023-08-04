46414602ca9d88cf8b78eebb83b3838896118321
LuxerIThink <eliteluxer@gmail.com>
2023-08-04 02:24:14 +0200

    :recycle: Refactor exception handlers

--- api/internal/exceptions_handlers.py
+++ api/internal/exceptions_handlers.py
@@ -1,6 +1,6 @@
 from starlette.responses import JSONResponse
 
 
-async def validation_exception_handler(_, exc):
-    error = {"loc": [str(exc).split(":")[0], 1], "msg": str(exc), "type": "validation"}
+async def validation_exception_handler(type, exc):
+    error = {"loc": [str(exc).split(":")[0], 1], "msg": str(exc), "type": str(type)}
     return JSONResponse(status_code=422, content={"detail": [error]})
