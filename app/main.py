dc2fd855c04ca060d583852f017964ce1e350f68
LuxerIThink <eliteluxer@gmail.com>
2024-05-27 08:45:21 +0200

    :art: Refactor routers init

--- app/main.py
+++ app/main.py
@@ -5,20 +5,16 @@ from app.internal.exceptions_handlers import validation_exception_handler
 from tortoise.exceptions import ValidationError
 from jose import JWTError
 from argon2.exceptions import VerifyMismatchError
+from importlib import import_module
 
 
 app = FastAPI()
 connect_to_db(app)
 Tortoise.init_models(["app.models"], "app")
 
-# WARNING! This import must be here because
-# Pydantics must be initialized after Tortoise early init to
-# correct work relations in Pydantics
-from .routers import actions, users, devices
-
-app.include_router(actions.router)
-app.include_router(users.router)
-app.include_router(devices.router)
+app.include_router(import_module("app.routers.actions").router)
+app.include_router(import_module("app.routers.users").router)
+app.include_router(import_module("app.routers.devices").router)
 
 app.add_exception_handler(ValidationError, validation_exception_handler)
 app.add_exception_handler(JWTError, validation_exception_handler)
