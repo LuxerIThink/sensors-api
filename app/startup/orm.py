4bb76d6bb20e02fc377844a1f636ef45a30e738f
LuxerIThink <luxerthegamer@gmail.com>
2025-01-19 23:14:31 +0100

    add: k8s

--- app/startup/orm.py
+++ app/startup/orm.py
@@ -1,9 +1,18 @@
 from tortoise.contrib.fastapi import register_tortoise
 from os import getenv
 from fastapi import FastAPI
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def connect_to_db(app: FastAPI) -> None:
+    host = str(getenv("DB_HOST") or "0.0.0.0")
+    port = str(getenv("DB_PORT") or "5432")
+    user = str(getenv("DB_USER"))
+    password = str(getenv("DB_PASSWORD"))
+    db = str(getenv("DB_NAME") or "postgres")
+    logger.exception(f"host: {host}, port: {port}, user: {user}, password: {password}, db: {db}")
     register_tortoise(
         app,
         config={
@@ -11,11 +20,11 @@ def connect_to_db(app: FastAPI) -> None:
                 "default": {
                     "engine": "tortoise.backends.asyncpg",
                     "credentials": {
-                        "host": str(getenv("DB_HOST")),
-                        "port": str(getenv("DB_PORT")),
-                        "user": str(getenv("DB_USER")),
-                        "password": str(getenv("DB_PASSWORD")),
-                        "database": str(getenv("DB_NAME")),
+                        "host": host,
+                        "port": port,
+                        "user": user,
+                        "password": password,
+                        "database": db,
                     },
                 }
             },
